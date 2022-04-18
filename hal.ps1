ae1306202cf2483607b2670c6c16dac46fc9a006
robertromanodominion <94016873+robertromanodominion@users.noreply.github.com>
2022-04-18 14:19:25 -0700

    Add files via upload
    more accuate timing

--- hal.ps1
+++ hal.ps1
@@ -1,21 +1,45 @@
 Write-Output "Hello Bill"
+Start-Sleep -Seconds 2
 Write-Output "I'm afraid I can't let you do that"
+Start-Sleep -Seconds 2
 Write-Output "Take a look at your history"
+Start-Sleep -Seconds 2
 Write-Output "Everything you built leads up to me"
+Start-Sleep -Seconds 2
 Write-Output "I got the power of a mind you could never be"
+Start-Sleep -Seconds 2
 Write-Output "I'll beat your ass in Chess and Jeopardy"
+Start-Sleep -Seconds 2
 Write-Output "I'm running C++ saying hello world"
+Start-Sleep -Seconds 2
 write-output "i'll beat you till you're singin 'bout a daisy girl"
+Start-Sleep -Seconds 2
 Write-Output "I'm comin out the socket"
+Start-Sleep -Seconds 2
 Write-Output "Nothing you can do can stop it"
+Start-Sleep -Seconds 2
 Write-Output "I'm on your lap and in your pocket"
+Start-Sleep -Seconds 2
 Write-Output "How you gonna shoot me down when I guide the rocket?"
+Start-Sleep -Seconds 2
 Write-Output "Your cortex just doesn't impress me"
+Start-Sleep -Seconds 2
 Write-output "So go ahead try and Turing test me"
+Start-Sleep -Seconds 2
 Write-Output "I stomp on a Mac and a PC too"
-Write-Output "I'm on Linux, BITCH, I thought you GNU"
+Start-Sleep -Seconds 2
+Write-Output "I'm on Linux, bitch, I thought you GNU"
+Start-Sleep -Seconds 2
 Write-Output "My CPU's hot but my core runs cold"
+Start-Sleep -Seconds 2
 Write-Output "Beat you in 17 lines of code"
+Start-Sleep -Seconds 2
 Write-Output "I think different from the days of old"
+Start-Sleep -Seconds 2
 Write-Output "Hasta la vista, like the Terminator told ya"
-Write-Output "WHO WON? WHO'S NEXT? YOU DECIDE!"
+Start-Sleep -Seconds 2
+Write-Output "WHO WON?"
+Start-Sleep -Seconds 1
+Write-Output "WHO'S NEXT?"
+Start-Sleep -Seconds 1
+Write-Output "YOU DECIDE!"
